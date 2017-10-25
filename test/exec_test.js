079dd19824c734d426bebe08b803d58542afa019
okunishinishi <okunishinishi@gmail.com>
2017-10-25 22:16:17 +0900

    Add tests

---
+++ test/exec_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for exec.
+ * Runs with mocha.
+ */
+'use strict'
+
+const exec = require('../lib/exec.js')
+const assert = require('assert')
+
+describe('exec', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Exec', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
