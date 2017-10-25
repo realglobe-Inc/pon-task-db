e1cf1338d4d1830e7e641a5da3b076e55940a777
okunishinishi <okunishinishi@gmail.com>
2017-10-25 12:30:15 +0900

    Support dump

--- test/migrate_test.js
+++ test/migrate_test.js
@@ -5,7 +5,6 @@
 'use strict'
 
 const migrate = require('../lib/migrate.js')
-const assert = require('assert')
 const theDB = require('the-db')
 const ponContext = require('pon-context')
 
@@ -21,19 +20,35 @@ describe('migrate', function () {
   })
 
   it('Migrate', async () => {
-    const db = theDB({})
+    const db = theDB({
+      dialect: 'sqlite',
+      storage: `${__dirname}/../tmp/testing-migration/test-data.db`
+    })
+
+    const Hoge = db.resource('Hoge')
+    await Hoge.create({
+      name: 'hoge'
+    })
     const ctx = ponContext()
 
     const task = migrate(() => db, {
       async 'none' (db) {
+        console.log('none')
         await db.updateVersion('hoge')
       },
       async 'hoge' () {
+        console.log('hoge')
         await db.updateVersion('foo')
       }
+    }, {
+      // snapshot: `${__dirname}/../tmp/testing-migration/snapshot`
     })
 
     await task(ctx)
+    await task(ctx)
+
+    await db.drop()
+
   })
 })
 
